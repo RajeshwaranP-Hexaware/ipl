890da2535ca0fe97a0a824d9047201f8900e5043
Rajeshwaran <rajeshfederer2@gmail.com>
2018-06-27 11:07:45 +0530

    first commit

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ app.post('/', (req, res) => {
     return res.json({statusCode:200});
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 3000!'));
