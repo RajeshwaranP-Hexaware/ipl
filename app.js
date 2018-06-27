f0d3860e917996e09b419e37f41725b5225b17ac
Rajeshwaran <rajeshfederer2@gmail.com>
2018-06-27 12:20:24 +0530

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express()
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.post('/', (req, res) => {
-    console.log('REQUSST', req);
+    console.log('REQUSST', req.body);
     return res.json({statusCode:200});
 });
 
