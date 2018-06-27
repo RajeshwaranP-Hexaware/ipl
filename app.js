410c0b651fbe43f5d490b3f548dc20fe756ffa01
Rajeshwaran <rajeshfederer2@gmail.com>
2018-06-27 11:03:41 +0530

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { json } from './C:/Users/39850/AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/body-parser';
+
 'use strict';
 
 const express = require('express');
@@ -5,6 +7,7 @@ const app = express()
 
 app.post('/', (req, res) => {
     console.log('REQUSST', JSON.stringify(req));
+    return res.json({statusCode:200});
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
