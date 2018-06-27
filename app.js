d524c35699823d64895609b207b27f4d01de1877
Rajeshwaran <rajeshfederer2@gmail.com>
2018-06-27 12:33:14 +0530

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { read } from 'fs';
+
 'use strict';
 
 const express = require('express');
@@ -9,7 +11,20 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 app.post('/', (req, res) => {
     console.log('REQUSST', req.body);
-    return res.json({statusCode:200});
+    if(req.body.queryResult && req.body.queryResult.action == "bookTicket"){
+        let params = req.body.parameters;
+        return res.json({
+            speech: "Speech- Tickets Booked",
+            displayText:"Display Speech- Tickets Booked",
+            messages: [{
+              "type": 0,
+              "platform": "facebook",
+              "speech": params.noOfTickets + " Tickets are booked for " + params.homeTeam + " vs " + params.awayTeam
+            }]
+        });
+    } else{
+        return res.json({statusCode:200});
+    }
 });
 
 app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 3000!'));
