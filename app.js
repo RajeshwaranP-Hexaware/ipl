2901763e2528f936b14802f5d09c34cac3d5c696
Rajeshwaran <rajeshfederer2@gmail.com>
2018-07-06 12:43:35 +0530

    trying userStorage

--- app.js
+++ app.js
@@ -15,33 +15,72 @@ app.post('/', (req, res) => {
       let params = req.body.queryResult.parameters;
       let userStorage = req.body.originalDetectIntentRequest.payload.user.userStorage ? JSON.parse(req.body.originalDetectIntentRequest.payload.user.userStorage) : {};
       userStorage.leagueTeam = params.leagueTeam;
+      console.log('STORAGE', userStorage);
       return res.json({
-        "fulfillmentText": "This is a text response",
-        "fulfillmentMessages": [
-          {
-            "card": {
-              "title": "card title",
-              "subtitle": "card text",
-              "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-              "buttons": [
-                {
-                  "text": "button text",
-                  "postback": "https://assistant.google.com/"
-                }
-              ]
+          "payload": {
+            "google": {
+              "expectUserResponse": true,
+              "richResponse": {
+                "items": [
+                  {
+                    "simpleResponse": {
+                      "textToSpeech": "Its Oh Great. Nice to know that you like "+ params.leagueTeam
+                    },
+                  }
+                ],
+                "suggestions": [
+                  {
+                    "title": "PremierLeague"
+                  },
+                  {
+                    "title": "FA Cup"
+                  },
+                  {
+                    "title": "Champions League"
+                  }
+                ]
+              },
+              "userStorage": JSON.stringify(userStorage)
             }
-          }
-        ],
-        "source": "example.com",
+        }
+      });
+
+    } else if(req.body.queryResult && req.body.queryResult.action == "getCountryTeamName"){
+      console.log('FEDERER ', JSON.stringify(req.body));
+      let params = req.body.queryResult.parameters;
+      let userStorage = req.body.originalDetectIntentRequest.payload.user.userStorage ? JSON.parse(req.body.originalDetectIntentRequest.payload.user.userStorage) : {};
+      userStorage.countryName = params.countryName;
+      console.log('STORAGE', userStorage);
+      return res.json({
         "payload": {
           "google": {
             "expectUserResponse": true,
             "richResponse": {
               "items": [
                 {
-                  "simpleResponse": {
-                    "textToSpeech": "this is a simple response"
+                  "basicCard":{
+                    "title": "Title",
+                    "subtitle": "Sub title",
+                    "formattedText": "Okay ?",
+                    "image": {
+                      "url": "https://images.pexels.com/photos/257840/pexels-photo-257840.jpeg?auto=compress&cs=tinysrgb&h=350",
+                      "accessibilityText": "ACCESS"
+                      // "height": number,
+                      // "width": number
+                    },
+                    "buttons": [{
+                      "title": "Submit"
+                      }
+                    ]
                   }
+                }
+              ],
+              "suggestions": [
+                {
+                  "title": "Worldcup"
+                },
+                {
+                  "title": "Continental cup"
                 }
               ]
             },
@@ -49,83 +88,54 @@ app.post('/', (req, res) => {
           }
         }
       });
-
-
-      // let params = req.body.queryResult.parameters;
-      // let userStorage = req.body.originalDetectIntentRequest.payload.user.userStorage ? JSON.parse(req.body.originalDetectIntentRequest.payload.user.userStorage) : {};
-      // userStorage.leagueTeam = params.leagueTeam;
-
-      // return res.json({
-      //   "userStorage": JSON.stringify(userStorage),
-      //   "finalResponse": {"speechResponse": {
-      //     "textToSpeech": "Its Oh Great. Nice to know that you like "+ params.leagueTeam ,
-      //     "ssml":  "<speak> </speak>"
-      //   }}
-      // });
-    } else if(req.body.queryResult && req.body.queryResult.action == "getCountryTeamName"){
-      console.log('FEDERER ', JSON.stringify(req.body));
-      let params = req.body.queryResult.parameters;
-      let userStorage = req.body.originalDetectIntentRequest.payload.user.userStorage ? JSON.parse(req.body.originalDetectIntentRequest.payload.user.userStorage) : {};
-      userStorage.countryName = params.countryName;
-
-      return res.json({
-        "userStorage": JSON.stringify(userStorage),
-        "finalResponse": {"speechResponse": {
-          "textToSpeech": "Its "+ params.countryName+" will do it.",
-          "ssml": "<speak> </speak>"
-        }}
-      });
     } else if(req.body.queryResult && req.body.queryResult.action == "bookTicket"){
         console.log('UNIQLO FEDERER ', JSON.stringify(req.body));
         let params = req.body.queryResult.parameters;
         let userStorage = req.body.originalDetectIntentRequest.payload.user.userStorage ? JSON.parse(req.body.originalDetectIntentRequest.payload.user.userStorage) : {};
         userStorage.homeTeam = params.homeTeam;
         userStorage.awayTeam = params.awayTeam;
+        console.log('STORAGE', userStorage);
         return res.json({
-          "userStorage": JSON.stringify(userStorage),
-          "finalResponse": {"speechResponse": {
-            "textToSpeech": "Its "+ params.homeTeam+" vs "+ params.awayTeam ,
-            "ssml": "<speak> </speak>"
-          }},
-          "expectUserResponse": true
+          "payload": {
+            "google": {
+              "expectUserResponse": true,
+              "richResponse": {
+                "items": [
+                  {
+                    "carouselBrowse":{
+                      "items": [
+                        {
+                          "title": "Carousal",
+                          "description": "description",
+                          "footer": "Footer",
+                          "image": {
+                            "url": "https://cdn.pixabay.com/photo/2017/01/06/23/21/soap-bubble-1959327_960_720.jpg"
+                          },
+                          "openUrlAction": {
+                            "url": "http://www.rf.com",   
+                          }
+                        }
+                      ],
+                      "imageDisplayOptions": ["WHITE"]
+                    }
+                  }
+                ],
+                "suggestions": [
+                  {
+                    "title": "CSK"
+                  },
+                  {
+                    "title": "FA Cup"
+                  },
+                  {
+                    "title": "Champions League"
+                  }
+                ]
+              },
+              "userStorage": JSON.stringify(userStorage)
+            }
+          } 
         });
-
-        // if(params.noOfTickets >10){
-        //   console.log('No Of Tickets ', params.noOfTickets);
-        //   return res.json({
-        //     "fulfillmentText": "Number of tickets should be below 10",
-        //     "fulfillmentMessages": [
-        //       {
-        //         "text": {
-        //           "text": [
-        //             "No. of tickets should be below 10"
-        //           ]
-        //         }
-        //       }
-        //     ],
-        //     "followupEventInput": {
-        //       "name": "customNoOfTickets",
-        //       "languageCode": "en-US",
-        //       "parameters": {
-        //         "awayTeam": params.awayTeam,
-        //         "homeTeam": params.homeTeam
-        //       }
-        //     }
-        // });
-        // } else{
-        //   return res.json({
-        //     "fulfillmentText": "Have a good time !",
-        //     "fulfillmentMessages": [
-        //       {
-        //         "text": {
-        //           "text": [
-        //             params.noOfTickets + " Tickets are booked for " + params.homeTeam + " vs " + params.awayTeam
-        //           ]
-        //         }
-        //       }
-        //     ]
-        //   });
-        // }
     } else{
         return res.json({statusCode:200});
     }
